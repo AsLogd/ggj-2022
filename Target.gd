d18fac7b3aaaa18f225ade9ee7da18565e1be6b7
aslogd <gerarddelcastillo@gmail.com>
2022-01-29 19:13:58 +0100

    fix target

--- Target.gd
+++ Target.gd
@@ -9,7 +9,7 @@ export var rotation_speed = 4
 
 # Called when the node enters the scene tree for the first time.
 func _process(delta):
-	transform = transform.rotated(Vector3(0, 0.1,0), rotation_speed*delta)
+	transform = transform.rotated(Vector3(0, 1,0), rotation_speed*delta)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
